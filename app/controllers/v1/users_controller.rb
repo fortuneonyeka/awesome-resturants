e5923895c555496bfc65aee0c0dd3c7288ea11fa
JuliCarracedo <julicarra@hotmail.com>
2022-01-28 16:44:46 -0300

    Create user controller and routes

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,15 @@
+class V1::User < ActionController::Base
+
+    def new
+
+    end
+    def create
+
+    end
+    def login
+
+    end
+    def logout
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :v1, defaults: {format: 'json'} do
+    resources :users, only: [:new, :create, :index] 
+    get "/login", to: "users#login", as: :user_login
+    delete "/logout", to: "users#logout", as: :create_session
+  end
+
 end
