370428e35edbd2a15b494567ad6b6b24c0da9824
JuliCarracedo <julicarra@hotmail.com>
2022-01-28 20:31:58 -0300

    add user creation endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   namespace :v1, defaults: {format: 'json'} do
-    resources :users, only: [:new, :create, :index] 
+    resources :users, only: [:create] 
     get "/login", to: "users#login", as: :user_login
     delete "/logout", to: "users#logout", as: :create_session
   end
