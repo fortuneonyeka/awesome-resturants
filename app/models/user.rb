4a813afade5015f71f98700e0d7cd040566b1003
JuliCarracedo <julicarra@hotmail.com>
2022-01-31 12:53:59 -0300

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :name, uniqueness: true, length: { in: 2...20 }
-   
+    validates :password_digest, length: { minimum: 2}
 end
