09f01b23211010deef92a7a26cedb527e9ae8851
JuliCarracedo <julicarra@hotmail.com>
2022-01-31 12:55:36 -0300

    add has_many relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    
+    has_many :todos, foreign_key: :created_by
 
     validates :name, uniqueness: true, length: { in: 2...20 }
     validates :password_digest, length: { minimum: 2}
