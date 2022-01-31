81795ad190915904c4e3e1e9a90b907d845005df
JuliCarracedo <julicarra@hotmail.com>
2022-01-31 13:22:19 -0300

    Add new login route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   
   namespace :v1, defaults: {format: 'json'} do
     resources :users, only: [:create] 
-    get "/login", to: "users#login", as: :user_login
     delete "/logout", to: "users#logout", as: :user_logout
   end
 
+  post 'auth/login', to: 'authentication#authenticate'
 end
