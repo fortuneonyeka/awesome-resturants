feff69397c3c9ab4c19479050042ca1c30281fda
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-02-03 23:03:45 +0100

    create reservations enpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     post 'auth/login', to: 'authentication#authenticate'
     post 'signup', to: 'users#create'
+
+    get '/reservations', to: 'reservations#index'
+    post '/reservations', to: 'reservations#create'
+    delete '/reservations/:id', to: 'reservations#destroy'
   end
 end
