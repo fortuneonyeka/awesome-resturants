fe4f852c4bb504299cbe6d89ee8e81803a9fccbe
JuliCarracedo <julicarra@hotmail.com>
2022-02-04 07:18:07 -0300

    add user seeds

--- db/migrate/20220128151322_create_users.rb
+++ db/migrate/20220128151322_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :password_digest
-
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+    User.create(name: 'Peter Johnson', password_digest: 'itspeter')
+    User.create(name: 'John Doe', password_digest: 'itsjohn')
+    User.create(name: 'JaneDoe', password_digest: '12345')
+    User.create(name: 'John Peters', password_digest: 'itsme')
+    User.create(name: 'Kim', password_digest: '123456')
