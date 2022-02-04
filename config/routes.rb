6dd4b832f008d2a7811426171a788ee47e6ce5f0
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-02-04 11:00:50 +0100

    update reservations routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
     post 'auth/login', to: 'authentication#authenticate'
     post 'signup', to: 'users#create'
 
-    get '/reservations', to: 'reservations#index'
-    post '/reservations', to: 'reservations#create'
-    delete '/reservations/:id', to: 'reservations#destroy'
+    # get "/posts/new", to: "posts#new"
+    # post '/reservations', to: 'reservations#create'
+    # get '/reservations', to: 'reservations#index'
+    # get "/reservations/:id/edit", to: "reservations#edit"
+    # put "/reservations/:id", to: "reservations#update" 
+    # delete '/reservations/:id', to: 'reservations#destroy'
+    resources :reservations
+  
   end
 end
