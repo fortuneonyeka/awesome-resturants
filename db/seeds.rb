8eec423324519c46c2e3e2c5a7340cf56b31fb6b
JuliCarracedo <julicarra@hotmail.com>
2022-02-04 07:24:11 -0300

    add restaurant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,9 @@
     User.create(name: 'JaneDoe', password_digest: '12345')
     User.create(name: 'John Peters', password_digest: 'itsme')
     User.create(name: 'Kim', password_digest: '123456')
+
+    Resturant.create(name: "Mason's", image:'me.me.com/url1', location: '5th Avenue No. 6774, Austin, Texas', description: "Come to Masons and enjoy the best meals of all Texas!", rating: 4)
+    Resturant.create(name: "MacDonald's", image:'me.me.com/url2', location: '5th Avenue No. 654, Austin, Texas', description: "Enjoy the best burgers in the market at the best price!", rating: 5)
+    Resturant.create(name: "Smelly Cat's Cove", image:'me.me.com/url1', location: '5th Avenue No. 6444, Austin, Texas', description: "-", rating: 2)
+
+  
