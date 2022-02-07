4f6500a3687573fd4cee70191881fdd7f2d3970e
JuliCarracedo <julicarra@hotmail.com>
2022-02-07 12:33:49 -0300

    Add reservations seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,33 @@
     Resturant.create(name: "MacDonald's", image:'me.me.com/url2', location: '5th Avenue No. 654, Austin, Texas', description: "Enjoy the best burgers in the market at the best price!", rating: 5)
     Resturant.create(name: "Smelly Cat's Cove", image:'me.me.com/url1', location: '5th Avenue No. 6444, Austin, Texas', description: "-", rating: 2)
 
-  
+    Reservation.create(
+        start_time: DateTime.new(2001,2,3,4),
+        end_time: DateTime.new(2001,2,3,5),
+        user_id: 1,
+        resturant_id: 2
+    )
+    Reservation.create(
+        start_time: DateTime.new(2001,2,3,13),
+        end_time: DateTime.new(2001,2,3,14),
+        user_id: 2,
+        resturant_id: 4
+    )
+    Reservation.create(
+        start_time: DateTime.new(2001,2,3,14),
+        end_time: DateTime.new(2001,2,3,15),
+        user_id: 2,
+        resturant_id: 5
+    )
+    Reservation.create(
+        start_time: DateTime.new(2001,2,3,13),
+        end_time: DateTime.new(2001,2,3,14),
+        user_id: 3,
+        resturant_id: 3
+    )
+    Reservation.create(
+        start_time: DateTime.new(2001,2,3,14),
+        end_time: DateTime.new(2001,2,3,15),
+        user_id: 3,
+        resturant_id: 4
+    )
