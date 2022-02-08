199d163151638a010abcd98b95ee92f2726b6506
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-02-08 10:00:54 +0100

    import reseration reducer

--- app/javascript/Redux/configureStore.js
+++ app/javascript/Redux/configureStore.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import ThunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
+import reservationReducer from './Reservations'
 
 
-const reducer = combineReducers({});
+const reducer = combineReducers({reservationReducer,});
 
 const store = createStore(reducer, applyMiddleware(ThunkMiddleware,logger));
 
