8033aaf489efe879787ff081e968c2337ad53921
JuliCarracedo <julicarra@hotmail.com>
2022-02-10 20:20:24 -0300

    add create restaurant endpoint

--- app/controllers/v1/restaurants_controller.rb
+++ app/controllers/v1/restaurants_controller.rb
@@ -7,4 +7,22 @@ class V1::RestaurantsController < ApplicationController
       restaurants: @restaurants
     }, status: 200
   end
+
+  def create
+    @restaurant = Resturant.new( restaurant_params )
+
+    if @restaurant.save
+      render json: {message: "Restaurant has been created"}, status: 200
+    else
+      render json: {errors: @restaurant.errors}, status: 422
+    end
+  end
+  
+  private
+  
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :rating, :image, :location)
+  end
+
 end
+
